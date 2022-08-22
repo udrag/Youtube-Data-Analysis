2b8330a229845afb854472f49e48ebdc4290cdc8
Dragos Ungureanu <dragosungureanu90@gmail.com>
2022-08-22 12:50:39 +0300

    Updated the function to youtube_trending_videos
    Moved the input instructions within the youtube_trending_videos() that were set to be printed before each of the input functions. The instructions were initially placed above the input function and were supposed to instruct the user what to input. Since there are multiple tools that read code, a better place was found for the instructions and names inside the input function. This will help those who use tools similar to PyCharm, as the input instructions will be shown directly on the Jupyter Input Request window.

--- yt_most_popular_videos_report.py
+++ yt_most_popular_videos_report.py
@@ -10,50 +10,45 @@ import googleapiclient.errors
 from googleapiclient.discovery import build
 
 # Get YouTube API credentials
-api_key = 'AIzaSyDTyW6xXRO6-Z8T5tXWLwKXv0SFmYlxfjI'
+api_key = 'YOUR API KEY'
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-def youtube_videos():
+def youtube_trending_videos():
     """
     This function generates a data frame with statistics about YouTube videos.
     The values of region code and number of results are obtained via input functions.
     In any input field write 'list' to get the list of all the region codes.
     Last question will ask the user for input on adding to the data frame the statistics about the channels of the videos.
     """
-    ## Create an API client for the region codes
+    # Create an API client for the region codes
     region_list = youtube.i18nRegions().list(part="snippet")
     response = region_list.execute()
     
-    ## User input for the region code
-    print('Enter the 2 letters of the region code. If you would like to see the whole list of region codes, please enter \'list\'')
-    region_code = str(input()).upper()
+    # User input for the region code
+    region_code = str(input('Enter the 2 letters of the region code. If you would like to see the whole list of region codes, please enter \'list\'')).upper()
     matches = str(bool([item for item in response['items'] if(item["id"] == region_code)]))
     
-    ## Conditional statement for the region codes list
+    # Conditional statement for the region codes list
     if region_code == 'LIST':
         for i in range(len(response['items'])):
             print(response['items'][i]['snippet']['gl'] + ' ' + '-' + ' ' + response['items'][i]['snippet']['name'])
-        print('Enter the 2 letters of the region code:')
-        region_code = str(input()).upper()
+        region_code = str(input('Enter the 2 letters of the region code:')).upper()
         matches = str(bool([item for item in response['items'] if(item["id"] == region_code)]))
     
-    ## Conditional statement for misspelled words
+    # Conditional statement for misspelled words
     while matches == 'False':
-        print(f'{region_code} is not a valid region code. Please enter the 2 letters of the region code. If you would like to see the whole list of region codes, please enter \'list\'')
-        region_code = str(input()).upper()
+        region_code = str(input(f'{region_code} is not a valid region code. Please enter the 2 letters of the region code. If you would like to see the whole list of region codes, please enter \'list\'')).upper()
         matches = str(bool([item for item in response['items'] if(item["id"] == region_code)]))
         if region_code == 'LIST': # Conditional statement for the region codes list inside the while loop
             for i in range(len(response['items'])):
                 print(response['items'][i]['snippet']['gl'] + ' ' + '-' + ' ' + response['items'][i]['snippet']['name'])
-            print('Enter the 2 letters of the region code:')
-            region_code = str(input()).upper()
+            region_code = str(input('Enter the 2 letters of the region code:')).upper()
             matches = str(bool([item for item in response['items'] if(item["id"] == region_code)]))
     
-    ## User input for the number of results
-    print('Enter the number of videos you would like to see (max 50).')
-    number = int(input())
+    # User input for the number of results
+    number = int(input('Enter the number of videos you would like to see (max 50).'))
       
-    ## API request parameters
+    # API request parameters
     search_response = youtube.videos().list(
                         part = 'snippet,contentDetails,statistics,localizations',
                         chart = 'mostPopular',
@@ -61,7 +56,7 @@ def youtube_videos():
                         maxResults = number)
     response = search_response.execute() 
     
-    ## Create a list and add the API results
+    # Create a list and add the API results
     all_data = []
     for i in range(len(response['items'])):
         try:
@@ -79,36 +74,31 @@ def youtube_videos():
 
         all_data.append(data)
         
-    ## User input for channel statistics to be adde to the data frame
-    print('Would you like to include channel info as well?')
-    answer = str(input()).upper()
-    
-    ## Conditional statement for YES answer
+    # User input for channel statistics to be adde to the data frame
+    answer = str(input('Would you like to include channel info as well?')).upper()
+
+    # Conditional statement for YES answer
     if answer == 'YES':
         channel_list = list((pd.DataFrame(all_data))['channel_id'])
-                
-        request = youtube.channels().list(
-                    part='snippet,contentDetails,statistics',
-                    id=','.join(channel_list))
+        request = youtube.channels().list(part='snippet,contentDetails,statistics',
+                                            id=','.join(channel_list))
         response = request.execute()
-        
         channel_ids = []      
         for i in range(len(response['items'])):
             try:
                 data = dict(channel_name = response['items'][i]['snippet']['title'],
-                            channel_subscribers = int(response['items'][i]['statistics']['subscriberCount']),
-                            channel_views = int(response['items'][i]['statistics']['viewCount']),
-                            channel_videos = int(response['items'][i]['statistics']['videoCount']),
-                            channel_id = response['items'][i]['id'])
-            
-                channel_ids.append(data)
-            
-            except KeyError:
-                print('A KeyError %d occurred:\n%s')
+                channel_subscribers = int(response['items'][i]['statistics']['subscriberCount']),
+                channel_views = int(response['items'][i]['statistics']['viewCount']),
+                channel_videos = int(response['items'][i]['statistics']['videoCount']),
+                channel_id = response['items'][i]['id'])
                 
-        ### Return the merged data
+                channel_ids.append(data)
+                
+            except KeyError:
+                print('A KeyError %d occurred:\n%s')      
+        # Return the merged data
         return pd.merge(pd.DataFrame(all_data), pd.DataFrame(channel_ids), on='channel_id')
-    ## Return the video statistics only
+    # Return the video statistics only
     else:
         return pd.DataFrame(all_data)
         
